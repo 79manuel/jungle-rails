9fea58c502603e18c851e8603ec1d5b0f0b77c0d
Manuel <prezcabellomanuel@gmail.com>
2017-07-07 01:46:41 +0000

    Added order details page

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -5,5 +5,9 @@ class LineItem < ActiveRecord::Base
 
   monetize :item_price_cents, numericality: true
   monetize :total_price_cents, numericality: true
-
+  def get_product_name
+    @order.line_items.each do |item|
+       @product = item.product.name
+    end
+  end
 end
