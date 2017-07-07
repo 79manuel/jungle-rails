162d0f2e4052f94668a2910c32dd374f0526b83f
Manuel <prezcabellomanuel@gmail.com>
2017-07-07 01:22:41 +0000

    Added sign up, sign in, log out

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,19 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+  resources :users , only: [:new, :create]
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#destroy'
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
